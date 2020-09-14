564b6155093517c7e27fb7d9b5646dd6e69b5555
Henry Hudson <henryhudson1431@yahoo.com>
2020-09-14 19:00:51 +0100

    json working

--- BinkIOSDevTest/ListOfCategories.swift
+++ BinkIOSDevTest/ListOfCategories.swift
@@ -8,22 +8,41 @@
 import SwiftUI
 
 struct ListOfCategories: View {
-    //@State var jsonData = [CategoriesJSON]()
-    @State var categories = [String]()
+    @State private var catInfo: [CategoryInfo] = [CategoryInfo]()
+    @State private var catNames: [String] = []
+    @State private var catImages: [String] = []
+    @State private var catDescriptions: [String] = []
     
     var body: some View {
         ZStack {
-            ForEach(0 ..< categories.count) { i in
-                Text(categories[i] )
+            VStack {
+                if catInfo.count > 0 {
+                    
+                    Text(catInfo[0].strCategory)
+                    
+                    RemoteImage(url: catInfo[0].strCategoryThumb, loading: Image(systemName: "icloud"), failure: Image(systemName: "xmark.circle"))
+                        .frame(width: 100, height: 100, alignment: .center)
+                    Text(catInfo[0].strCategoryDescription)
+                    
+                }
             }
         }
         .onAppear {
             withAnimation(Animation.linear(duration: 0.1)) {
-                getJSON(urlString: "https://www.themealdb.com/api/json/v1/1/categories.php") { (data: CategoryInfo?) in
+                getJSON(urlString: "https://www.themealdb.com/api/json/v1/1/categories.php") { (data: CategoriesJSON?) in
                     if let data = data {
-                        categories.append(data.strCategory)
+                        
+                        catInfo = data.categories
+                        
+                        
+                        for i in catInfo {
+                            catNames.append(i.strCategory)
+                            catImages.append(i.strCategoryThumb)
+                            catDescriptions.append(i.strCategoryDescription)
+                            
+                        }
+                        
                     }
-                    
                 }
             }
         }
