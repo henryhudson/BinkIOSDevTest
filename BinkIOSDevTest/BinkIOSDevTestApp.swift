791df61463a28675b62bd60f65040a83adc489dc
Henry Hudson <henryhudson1431@yahoo.com>
2020-09-14 13:26:09 +0100

    Initial Commit

---
+++ BinkIOSDevTest/BinkIOSDevTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinkIOSDevTestApp.swift
+//  BinkIOSDevTest
+//
+//  Created by Henry Hudson on 14/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct BinkIOSDevTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
